9b1d0d9639407720165106c74f41650547cbc32f
wyyaa123 <863114354@qq.com>
2023-01-19 00:16:34 +0800

    test

---
+++ build/CMakeFiles/opencv.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv.dir/main.cpp.obj"
+  "CMakeFiles/opencv.dir/main.cpp.obj.d"
+  "libopencv.dll.a"
+  "opencv.exe"
+  "opencv.exe.manifest"
+  "opencv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
