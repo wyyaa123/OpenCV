c6f12b2c0bb7f5c81e6b31aa3c4ce44fcf6e5bd4
wyyaa123 <863114354@qq.com>
2023-01-20 03:41:50 +0800

    test

---
+++ src/2.PlayVideo/main.cpp
@@ -0,0 +1,19 @@
+﻿// 2.PlayVideo.cpp: 定义应用程序的入口点。
+//
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+int main()
+{
+	cv::namedWindow("Example2", cv::WINDOW_AUTOSIZE);
+	cv::VideoCapture cap;
+	cap.open("C:\\Users\\南九的橘猫\\Desktop\\OpenCV\\videos\\video1.mp4");
+	cv::Mat frame;
+	for (;;) {
+		cap >> frame;
+		if (frame.empty()) break;
+		cv::imshow("Example2", frame);
+		if (cv::waitKey(30) >= 0) break;
+	}
+	return 0;
+}
